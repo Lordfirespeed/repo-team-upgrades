fd551f8701d60fa6f14619b191564436550e37d7
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-03-26 02:31:01 +0000

    working version dumper

---
+++ repo-playground/Plugin.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine.Device;
+
+namespace RepoPlayground;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static Plugin Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+
+    private CancellationTokenSource? _cts;
+
+    public Plugin()
+    {
+        if (Instance is not null) {
+            throw new InvalidOperationException($"{typeof(Plugin).AssemblyQualifiedName} was instantiated twice");
+        }
+        Instance = this;
+    }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        var harmony = new Harmony(Info.Metadata.GUID);
+        harmony.PatchAll(typeof(Patches));
+    }
+
+    private void OnDestroy()
+    {
+        if (_cts is { IsCancellationRequested: false }) _cts.Cancel();
+    }
+
+    private void DumpVersionAndExit(string versionString)
+    {
+        if (_cts is not null) {
+            throw new InvalidOperationException("DumpVersionAndExit has already been invoked");
+        }
+
+        _cts = new();
+        Task.Run(async () => await DumpVersionAndExit(versionString, _cts.Token), _cts.Token);
+    }
+
+    private async Task DumpVersionAndExit(string versionString, CancellationToken ct)
+    {
+        await DumpVersion(versionString, ct);
+        if (ct.IsCancellationRequested) return;
+        Application.Quit();
+    }
+
+    private async Task DumpVersion(string versionString, CancellationToken ct)
+    {
+        var pluginDirectory = Path.GetDirectoryName(Info.Location)!;
+        var versionDumpFile = Path.Combine(pluginDirectory, "version.txt");
+        await using var versionDumpStream = new StreamWriter(versionDumpFile);
+        await versionDumpStream.WriteLineAsync(versionString);
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    class Patches
+    {
+        [HarmonyPatch(typeof(BuildManager), nameof(BuildManager.Awake))]
+        [HarmonyPostfix]
+
+        static void AfterBuildManagerWakes(BuildManager __instance)
+        {
+            if (!ReferenceEquals(__instance, BuildManager.instance)) {
+                return;
+            }
+
+            var versionString = BuildManager.instance.version.title;
+            Plugin.Logger.LogInfo($"Version information: {versionString}");
+            Plugin.Instance.DumpVersionAndExit(versionString);
+        }
+    }
+}
