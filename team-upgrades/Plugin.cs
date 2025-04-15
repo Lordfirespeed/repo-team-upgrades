428186bb61fa14680d9c62a3e2039bff12a43674
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-16 00:28:06 +0100

    clean out unneeded stuff + adjust names

---
+++ team-upgrades/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine.Device;
+
+namespace TeamUpgrades;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static Plugin Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+
+    public Plugin()
+    {
+        if (Instance is not null) {
+            throw new InvalidOperationException($"{typeof(Plugin).AssemblyQualifiedName} was instantiated twice");
+        }
+        Instance = this;
+    }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        var harmony = new Harmony(Info.Metadata.GUID);
+        harmony.PatchAll(typeof(Patches));
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    class Patches
+    {
+
+    }
+}
