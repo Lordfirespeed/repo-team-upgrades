990cecdb5357d8fa8f782a4121b2aad3d71f85dd
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-17 00:12:42 +0100

    add `SimpleEnumerator` to `HarmonyExtensions` to help patching enumerators

--- team-upgrades/Extensions/HarmonyExtensions.cs
+++ team-upgrades/Extensions/HarmonyExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
 
@@ -26,6 +28,33 @@ public static class HarmonyExtensions
         {
             harmony.PatchAll(nestedType);
             PatchAllNestedTypesRecursively(harmony, nestedType);
+        }
+    }
+
+    // https://gist.github.com/pardeike/c873b95e983e4814a8f6eb522329aee5
+    public class SimpleEnumerator<T> : IEnumerable<T>
+    {
+        public required IEnumerator<T> Enumerator { get; init; }
+        public Action PrefixAction { get; init; } = () => { };
+        public Action PostfixAction { get; init; } = () => { };
+        public Action<T> PreItemAction { get; init; } = item => { };
+        public Action<T> PostItemAction { get; init; } = item => { };
+        public Func<T, T> ItemAction { get; init; } = item => item;
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() { return _GetEnumerator(); }
+        IEnumerator IEnumerable.GetEnumerator() { return _GetEnumerator(); }
+
+        private IEnumerator<T> _GetEnumerator()
+        {
+            PrefixAction();
+            while (Enumerator.MoveNext())
+            {
+                var item = Enumerator.Current;
+                PreItemAction(item);
+                yield return ItemAction(item);
+                PostItemAction(item);
+            }
+            PostfixAction();
         }
     }
 }
