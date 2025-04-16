0dba4a746ed1ecdeb0459a346ca24ddd8e9927e7
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-04-16 22:21:02 +0100

    fix: `init`-only, not `set`

--- team-upgrades/UpgradeQuantityChangedEventArgs.cs
+++ team-upgrades/UpgradeQuantityChangedEventArgs.cs
@@ -4,7 +4,7 @@ namespace TeamUpgrades;
 
 public class UpgradeQuantityChangedEventArgs : EventArgs
 {
-    public required string PlayerSteamId { get; set; }
-    public required string StatsKey { get; set; }
-    public required int Quantity { get; set; }
+    public required string PlayerSteamId { get; init; }
+    public required string StatsKey { get; init; }
+    public required int Quantity { get; init; }
 }
